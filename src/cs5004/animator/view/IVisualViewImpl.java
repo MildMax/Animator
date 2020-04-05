4943e5d7dc2f52abac12328e0eefa38b8f3afade
MildMax <joseph.s.burns93@gmail.com>
2020-04-05 16:27:45 -0400

    made new IVisualViewImpl for new class in assignment 3

---
+++ src/cs5004/animator/view/IVisualViewImpl.java
@@ -0,0 +1,36 @@
+package cs5004.animator.view;
+
+import java.util.List;
+
+import cs5004.animator.model.AnimationModel;
+import cs5004.animator.model.shapes.Shape;
+
+public class IVisualViewImpl implements AnimationView {
+
+
+
+  @Override
+  public void openView() {
+
+  }
+
+  @Override
+  public void closeView() {
+
+  }
+
+  @Override
+  public void run(AnimationModel m) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public void drawNewFrame(List<Shape> shapeList) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public String getOutFileContents() {
+    return null;
+  }
+}
