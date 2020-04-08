ea49a19787aab895cee6a7f9186116631fc9ecab
MildMax <joseph.s.burns93@gmail.com>
2020-04-07 22:53:34 -0400

    Added comments to controller classes.

--- src/cs5004/animator/controller/TogglePlayMouseListener.java
+++ src/cs5004/animator/controller/TogglePlayMouseListener.java
@@ -5,14 +5,35 @@ import java.awt.event.MouseEvent;
 
 import cs5004.animator.view.AnimationRunner;
 
+/**
+ * The TogglePlayMouseListener listens for clicks on the AnimationView window and changes the
+ * state of the Animation. Extends the MouseAdapter class.
+ */
 public class TogglePlayMouseListener extends MouseAdapter {
 
   AnimationRunner runner;
 
-  public TogglePlayMouseListener(AnimationRunner runner) {
+  /**
+   * Takes an AnimationRunner that is running the current animation and stores it in the
+   * current object.
+   *
+   * @param runner takes an AnimationRunner displaying the current animation.
+   * @throws IllegalArgumentException
+   */
+  public TogglePlayMouseListener(AnimationRunner runner) throws IllegalArgumentException {
+    if (runner == null) {
+      throw new IllegalArgumentException("AnimationRunner cannot be null");
+    }
     this.runner = runner;
   }
 
+  /**
+   * When the mouse is clicked, changes the play state of the animation in the AnimationRunner.
+   * If the animation is playing, pauses the animation. If the animation is paused, plays the
+   * animation.
+   *
+   * @param e takes a MouseEvent tied to the window that displays the animation.
+   */
   @Override
   public void mouseClicked(MouseEvent e) {
     super.mouseClicked(e);
